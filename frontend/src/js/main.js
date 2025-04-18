6b3f32b260c1df347a08e780b24b0b6fd1cf2fde
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-04-18 17:26:08 +0530

    Update the CORS policy impl in frontend

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -1,2 +1,25 @@
 import $ from 'jquery';
 
+
+loadAllCustomers();
+
+function loadAllCustomers(){
+
+    // 1.
+    const xhr = new XMLHttpRequest();
+
+    // 2.
+    xhr.addEventListener('readystatechange', ()=>{
+        console.log("Call back fn");
+    });
+
+    // 3.
+    xhr.open('GET',
+        'http://localhost:8080/app/v1/customers', true);
+
+    // 4.
+
+    // 5.
+    xhr.send();
+
+}
